9d3a395f6c764c46f2fb98f917981abb2063a107
farhan <mohdfarhanfahrezy@gmail.com>
2023-11-09 00:34:09 +0700

    feat: setup docker to soap and db

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# Use Corretto 1.8 as the base image
+FROM amazoncorretto:8
+
+WORKDIR /app
+
+# Copy the packaged JAR file into the image
+COPY target/SOAP-0.0-jar-with-dependencies.jar /app/SOAP-0.0-jar-with-dependencies.jar
+
+# Expose the port your app runs on
+EXPOSE 6060
+
+# Set the command to run your app
+CMD ["java", "-jar", "SOAP-0.0-jar-with-dependencies.jar"]
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import javax.xml.ws.Endpoint;
 public class Main {
     public static void main(String[] args) {
         try {
-            Endpoint.publish("http://localhost:6969/ws/sabun", new TestingService());
+            Endpoint.publish("http://0.0.0.0:6060/ws/sabun", new TestingService());
             System.out.println("Gacor");
         } catch (Exception e){
             System.out.println("Something Wlong");
